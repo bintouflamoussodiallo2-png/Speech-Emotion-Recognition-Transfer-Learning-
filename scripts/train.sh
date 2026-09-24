7df19f28158deaf193730b84d17365dbd8e797af
Bintou Flamousso Diallo <ebd4258@umoncton.ca>
2026-09-24 00:42:18 -0300

    slurm job add

---
+++ scripts/train.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+#SBATCH --job-name=SER_train 
+#SBATCH --account=ebd4258 
+#SBATCH --gres=gpu:H100:1 
+#SBATCH --cpu-per-task=16 
+#SBATCH --mem=32G
+#SBATCH --mail-user=ebd4258@umoncton.ca
+#SBATCH --mail-type=END,FAIL
+#SBATCH --time=02:00:00
+#SBATCH --output=results/SER_%j.log 
+
+
+source .venv/bin/activate
+
+MODEL=$1
+DATASET=$2
+DATA_PATH=$3
+
+echo "-------------------------"
+echo " Job ID      : $SLURM_JOB_ID"
+echo " Model       : $MODEL"
+echo " Dataset     : $DATASET"
+echo " Data path   : $DATA_PATH"
+echo " GPU         : $CUDA_VISIBLE_DEVICES"
+echo "-------------------------"
+
+cd $SLURM_SUBMIT_DIR
+
+python -m src.train \
+    --model $MODEL \
+    --dataset $DATASET \
+    --data-path $DATA_PATH \
+    --epochs 20 \
+    --batch-size 64 \
+    --lr 1e-4
+
